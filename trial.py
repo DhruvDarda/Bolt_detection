2e534acf537cd950e8368d47f844846929705d7d
DhruvDarda <65066460+DhruvDarda@users.noreply.github.com>
2022-11-23 01:54:48 +0530

    Add files via upload

--- trial.py
+++ trial.py
@@ -2,6 +2,36 @@ import cv2
 import os
 import numpy as np
 
+original_horizontal_distance = 15
+original_vertical_distance = 12
+
+
+def output(result):
+    global image
+    gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    centers = []
+
+    ret, thresh = cv2.threshold(gray, 127, 255, 0)
+
+    # find contours in the binary image
+    contours, hierarchy = cv2.findContours(
+        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for c in contours:
+        # calculate moments for each contour
+        M = cv2.moments(c)
+
+        # calculate x,y coordinate of center
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        cv2.circle(image, (cX, cY), 5, (255, 255, 255), -1)
+        centers.append((cX, cY))
+
+    return centers
+
+
 os.chdir("E:/Sem 7/Robotics/Final_Project/")
 
 # Read image
@@ -20,44 +50,46 @@ upper_red = np.array([180, 255, 255])
 blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
 red_mask = cv2.inRange(hsv, lower_red, upper_red)
 
+
+coordinates = [0, 0]
 result = cv2.bitwise_and(image, image, mask=blue_mask)
-cv2.imshow("blue_filter", result)
-
-gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
-
-centroid_image = gray.shape
+centers = output(result)
+centroid_image = result.shape
 centroid_image = (centroid_image[1]//2, centroid_image[0]//2)
-print(centroid_image)
 
-gray = cv2.GaussianBlur(gray, (5, 5), 0)
+while len(centers) > 4:
+    vertical_dist_lst = [np.abs(centers[0][1] - centers[2][1]),
+                         np.abs(centers[1][1] - centers[3][1])]
 
-centers = []
+    horizontal_dist_lst = [np.abs(centers[0][0] - centers[1][0]),
+                           np.abs(centers[2][0] - centers[3][0])]
 
-ret, thresh = cv2.threshold(gray, 127, 255, 0)
+    horizontal_dist = np.mean(horizontal_dist_lst)
+    vertical_dist = np.mean(vertical_dist_lst)
 
-# find contours in the binary image
-contours, hierarchy = cv2.findContours(
-    thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-for c in contours:
-    # calculate moments for each contour
-    M = cv2.moments(c)
+    coordinates[0] = (
+        horizontal_dist/original_horizontal_distance) * (centers[4][1]-centroid_image[1])
+    coordinates[1] = (
+        vertical_dist/original_vertical_distance) * (centers[4][0]-centroid_image[0])
 
-    # calculate x,y coordinate of center
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
-    cv2.circle(image, (cX, cY), 5, (255, 255, 255), -1)
-    centers.append((cX, cY))
+result = cv2.bitwise_and(image, image, mask=red_mask)
+centers = output(result)
 
+while len(centers) > 4:
+    vertical_dist_lst = [np.abs(centers[0][1] - centers[2][1]),
+                         np.abs(centers[1][1] - centers[3][1])]
+
+    horizontal_dist_lst = [np.abs(centers[0][0] - centers[1][0]),
+                           np.abs(centers[2][0] - centers[3][0])]
+
+    horizontal_dist = np.mean(horizontal_dist_lst)
+    vertical_dist = np.mean(vertical_dist_lst)
+
+    coordinates[0] = (
+        horizontal_dist/original_horizontal_distance) * (centers[4][1]-centroid_image[1])
+    coordinates[1] = (
+        vertical_dist/original_vertical_distance) * (centers[4][0]-centroid_image[0])
 
 cv2.imshow("Final Image", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
-vertical_dist = [np.abs(centers[0][1] - centers[2][1]),
-                 np.abs(centers[1][1] - centers[3][1])]
-
-horizontal_dist = [np.abs(centers[0][0] - centers[1][0]),
-                   np.abs(centers[2][0] - centers[3][0])]
-
-print(np.mean(horizontal_dist), np.mean(vertical_dist))
