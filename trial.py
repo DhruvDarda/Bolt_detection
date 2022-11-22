c0d103dcd909b0d56e1f579d4c2312479dd22acf
DhruvDarda <65066460+DhruvDarda@users.noreply.github.com>
2022-11-23 00:44:08 +0530

    Add files via upload

--- trial.py
+++ trial.py
@@ -1,66 +1,63 @@
 import cv2
+import os
 import numpy as np
 
-image = cv2.imread("E:/Sem 7/Robotics/Final_Project/forlines.jpg")
+os.chdir("E:/Sem 7/Robotics/Final_Project/")
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# Read image
+image = cv2.imread("E:/Sem 7/Robotics/Final_Project/test.jpg")
 
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
+# Threshold of blue in HSV space
+lower_blue = np.array([60, 35, 140])
+upper_blue = np.array([180, 255, 255])
 
-contours, _ = cv2.findContours(
+lower_red = np.array([160, 50, 50])
+upper_red = np.array([180, 255, 255])
+
+# preparing the mask to overlay
+blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
+red_mask = cv2.inRange(hsv, lower_red, upper_red)
+
+result = cv2.bitwise_and(image, image, mask=blue_mask)
+cv2.imshow("blue_filter", result)
+
+gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+
+centroid_image = gray.shape
+centroid_image = (centroid_image[1]//2, centroid_image[0]//2)
+print(centroid_image)
+
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+centers = []
+
+ret, thresh = cv2.threshold(gray, 127, 255, 0)
+
+# find contours in the binary image
+contours, hierarchy = cv2.findContours(
     thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+for c in contours:
+    # calculate moments for each contour
+    M = cv2.moments(c)
 
-max_area = 0
-c = 0
-for i in contours:
-    area = cv2.contourArea(i)
-    if area > 1000:
-        if area > max_area:
-            max_area = area
-            best_cnt = i
-            image = cv2.drawContours(image, contours, c, (0, 255, 0), 3)
-    c += 1
-
-mask = np.zeros((gray.shape), np.uint8)
-cv2.drawContours(mask, [best_cnt], 0, 255, -1)
-cv2.drawContours(mask, [best_cnt], 0, 0, 2)
-
-out = np.zeros_like(gray)
-out[mask == 255] = gray[mask == 255]
-
-blur = cv2.GaussianBlur(out, (5, 5), 0)
-
-thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
-
-contours, _ = cv2.findContours(
-    thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-print(len(contours))
-
-c = 0
-for i in contours:
-    area = cv2.contourArea(i)
-    if area > 1000/2:
-        cv2.drawContours(out, contours, c, (0, 255, 0), 3)
-    c += 1
+    # calculate x,y coordinate of center
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+    cv2.circle(image, (cX, cY), 5, (255, 255, 255), -1)
+    centers.append((cX, cY))
 
 
-cv2.imshow("Final Image", out)
+cv2.imshow("Final Image", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
-'''
-def process_video(test_video, output_video):
-    """
-    Read input video stream and produce a video file with detected lane lines.
-        Parameters:
-            test_video: Input video.
-            output_video: A video file with detected lane lines.
-    """
-    input_video = VideoFileClip(os.path.join('test_videos', test_video), audio=False)
-    processed = input_video.fl_image(frame_processor)
-    processed.write_videofile(os.path.join('output_videos', output_video), audio=False)
-'''
+vertical_dist = [np.abs(centers[0][1] - centers[2][1]),
+                 np.abs(centers[1][1] - centers[3][1])]
+
+horizontal_dist = [np.abs(centers[0][0] - centers[1][0]),
+                   np.abs(centers[2][0] - centers[3][0])]
+
+print(np.mean(horizontal_dist), np.mean(vertical_dist))
